3ffd0babc6f973cd588413ccbe68f346ed436d83
Hassan Raza <hassanrsiddiqi@gmail.com>
2023-12-11 21:14:00 +0500

    update url for .yml

--- lib/mix/tasks/update_openai_docs.ex
+++ lib/mix/tasks/update_openai_docs.ex
@@ -10,7 +10,8 @@ defmodule Mix.Tasks.UpdateOpenaiDocs do
   @target_dir "lib/ex_openai/docs"
   def run(_) do
     File.mkdir_p!(@target_dir)
-    docs_url = "https://raw.githubusercontent.com/openai/openai-openapi/master/openapi.yaml"
+    # docs_url = "https://raw.githubusercontent.com/openai/openai-openapi/master/openapi.yaml"
+    docs_url = "https://raw.githubusercontent.com/hassanRsiddiqi/ex_openai/add_retrieve_message_function/lib/ex_openai/docs/docs.yaml"
     System.cmd("curl", ["-L", docs_url, "-o", "#{@target_dir}/docs.yaml"])
   end
 end
